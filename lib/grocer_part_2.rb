2556ae6dea45e289c4db689cbc8c74520a5c5540
Shelley Brook <sbrook13@gmail.com>
2020-07-30 00:14:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,6 +1,7 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
+  after_coupons = []
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
