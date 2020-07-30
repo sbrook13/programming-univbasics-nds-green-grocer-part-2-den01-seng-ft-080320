67ee18dfcd95320c665d8d0fdf44f4ad7512cb81
Shelley Brook <sbrook13@gmail.com>
2020-07-30 00:59:32 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -38,13 +38,16 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  consolidate_cart(cart)
+  apply_coupons(cart, coupons)
+  apply_clearance(cart)
+  subtotal = 0
+  i=0
+  while i < cart.length
+    subtotal = subtotal + (cart[i][:price]*cart[i][:count])
+    i += 1 
+  end 
+  if subtotal > 100
+    subtotal = (subtotal * 0.90).round(2)
+  end
 end
